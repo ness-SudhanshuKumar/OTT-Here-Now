44768ce18735ed96cd8193c431f078b4b0b83089
ness-SudhanshuKumar <p7114330@ness.com>
2025-12-29 20:39:01 +0530

    Remove port hardcoding

--- profile-service/src/main/java/com/sudhanshu/profileservice/controller/UserProfileController.java
+++ profile-service/src/main/java/com/sudhanshu/profileservice/controller/UserProfileController.java
@@ -1,4 +1,4 @@
-package com.sudhanshu.profileservice;
+package com.sudhanshu.profileservice.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
