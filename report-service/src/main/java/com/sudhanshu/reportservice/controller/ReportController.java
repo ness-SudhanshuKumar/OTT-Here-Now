7053873098a92a1d355c24df05dae987f5accc16
ness-SudhanshuKumar <p7114330@ness.com>
2026-01-06 09:18:38 +0530

    add auth server and CB

--- report-service/src/main/java/com/sudhanshu/reportservice/controller/ReportController.java
+++ report-service/src/main/java/com/sudhanshu/reportservice/controller/ReportController.java
@@ -4,11 +4,14 @@ import java.util.List;
 
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 
 
 @RestController
@@ -24,6 +27,7 @@ public class ReportController {
 	}
 	
 	@GetMapping("/event/stats")
+	@CircuitBreaker(name = "event-stats-cb", fallbackMethod = "eventStatsFallback")
 	public ResponseEntity<?> getEventStatus(){
 		//find event service instances
 		System.out.println("start /event/stats");
@@ -34,7 +38,10 @@ public class ReportController {
 		}
 		
 		ServiceInstance eventServiceInstance = instances.get(0);
+		System.out.println(eventServiceInstance.toString() + "  URI: " + eventServiceInstance.getUri());
+
         String eventUrl = "http://event-service/events";
+
 
 		//String eventUrl = eventServiceInstance.getUri() + "/events/aggregates";
 		
@@ -42,5 +49,12 @@ public class ReportController {
 		
 		ResponseEntity<?> stats = restTemplate.getForEntity(eventUrl, Object.class);
 		return stats;
+	}
+	
+	public ResponseEntity<?> eventStatsFallback(Exception ex){
+		
+		System.out.println("Event stats API is down: " + ex.getMessage());
+		
+		return new ResponseEntity<String>("Servie is down", HttpStatusCode.valueOf(503));
 	}
 }
